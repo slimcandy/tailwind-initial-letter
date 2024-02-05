f80208a45553dae5088f3548bc0ca523f827dd93
SlimCandy <91788772+slimcandy@users.noreply.github.com>
2024-02-05 18:23:48 +0100

    Resolve conflicts 3

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-export default function fontVariantCaps({ addUtilities, config, e }) {
-  const values = config("theme.fontVariantCaps", {
+export default function initialLetter({ addUtilities, config, e }) {
+  const values = config("theme.initialLetter", {
     normal: "normal",
-    small: "small-caps",
-    all: "all-small-caps",
-    petite: "petite-caps",
-    unicase: "unicase",
-    titling: "titling-caps",
+    initial: "initial",
+    1.5: "1.5",
+    2: "2",
+    3: "3",
+    4: "4",
   });
 
-  const variants = config("variants.fontVariantCaps", []);
+  const variants = config("variants.initialLetter", []);
   const utilities = {};
 
   for (const key in values) {
-    utilities[`.font-variant-caps-${e(key)}`] = {
-      "font-variant-caps": values[key],
+    utilities[`.initial-letter-${e(key)}`] = {
+      "-webkit-initial-letter": values[key],
+      "initial-letter": values[key],
     };
   }
 
